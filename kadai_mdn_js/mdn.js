b2ba848a10f86c0de57aa176e68a7c42837448d6
Yuki3656 <yukiss515@gmail.com>
2024-07-10 20:07:48 +0900

    フォルダkadai_mdn_jsの修正をアップロード

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,10 +1,6 @@
-//日付データフォーマットの関数を定義
-function formatDate(date) {
-    const options = {year: 'numeric', month: 'long', day:'numeric'};
-    return date.toLocaleDateString('ja-JP', options);
-}
-
 //本日の日付を出力
 const today = new Date();
-const formattedDate = formatDate(today);
-console.log(formattedDate)
+const year =today.getFullYear();
+const month =today.getMonth() + 1;
+const date =today.getDate();
+console.log(`${year}年${month}月${date}日`);
